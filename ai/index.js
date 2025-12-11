bd13707c40aee18b903dc0ebcb77f74dde691712
洪英鑫 <1104350235@qq.com>
2025-12-11 15:47:32 +0800

    feat: 测试提交

--- ai/index.js
+++ ai/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import { GoogleGenAI } from "@google/genai";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
-
+console.log('--------------')
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
